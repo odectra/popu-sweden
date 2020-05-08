8a3e0293b54b9b4dc6d7b3d59851ebd8b8386558
osksve <osvensson91@gmail.com>
2020-05-08 14:34:00 +0200

    Added new municipal data

--- newmap.py
+++ newmap.py
@@ -13,17 +13,18 @@ import os
 # Function to load the approriate json file. E.g. GBG = kom_GBG.json
 def load_map(data, fn):
     """This function will load the .json file 'fn' , from 'data' location """
-    o = os.path.join(data, fn)
-    return o
+    result = os.path.join(data, fn)
+    return result
+
+# select Geojson data using local load_map() function
+overlay = load_map('data', 'kom_ALI.json')
+
 
 #Create map object (with center point at location)
 m = folium.Map(location=[62.216915, 15.296679], zoom_start=5)
 
 # Global Tooltip
 tooltip = 'Click For More Info'
-
-# seect Geojson data using local load_map() function
-overlay = load_map('data', 'kom_GBG.json')
 
 # Create marker at Gothenburg
 folium.Marker([57.708711, 11.974598], 
