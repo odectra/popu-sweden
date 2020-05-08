ea0927e3c83bc4cab7abfa3788f47842131cef9c
osksve <osvensson91@gmail.com>
2020-05-08 16:34:09 +0200

    Updated newmap.py

--- newmap.py
+++ newmap.py
@@ -12,13 +12,15 @@ import os
 
 # Function to load the approriate json file. E.g. GBG = kom_GBG.json
 def load_map(data, fn):
-    """This function will load the .json file 'fn' , from 'data' location """
+    """This function will load the .json file 'fn' , from 'data' location using os package"""
     result = os.path.join(data, fn)
     return result
 
-# select Geojson data using local load_map() function
-overlay = load_map('data', 'kom_ALI.json')
+#Select municipality 
+kom_name='Bollnas' 
 
+# select Geojson data using local load_map() function
+overlay = load_map('data', 'kom_BOS.json')
 
 #Create map object (with center point at location)
 m = folium.Map(location=[62.216915, 15.296679], zoom_start=5)
@@ -33,8 +35,7 @@ folium.Marker([57.708711, 11.974598],
 		icon=folium.Icon(color= 'green', icon='heart')).add_to(m),
 		
 # Geojson overlay
-#folium.GeoJson(overlay, name='alingsas').add_to(m)
-folium.GeoJson(overlay, name='selected map').add_to(m)
+folium.GeoJson(overlay, name='selected map', tooltip=tooltip).add_to(m).add_child(folium.Popup(kom_name)).add_to(m)
 
 #Generate map - this will create an html-file to display file
 m.save('map.html')
