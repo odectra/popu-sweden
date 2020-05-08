eeb34cb185e038535410ec64c4f3f42e6055332d
osksve <osvensson91@gmail.com>
2020-05-08 10:17:54 +0200

    Created maps for alingsas using GeoJson - second push attempt

---
+++ map.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 09:44:43 2020
+
+@author: Oskar_Privatdator
+"""
+
+
+import folium
+
+#Imported for vega
+import os 
+import json
+
+#Create map object (with center point at location)
+m = folium.Map(location=[57.927733, 12.539132], zoom_start=8)
+
+# Global Tooltip
+tooltip = 'Click For More Info'
+
+#Geojson data
+overlay = os.path.join('data', 'kom_ALI.json')
+
+# Create markers 
+folium.Marker([57.927733, 12.539132], 
+		popup='<strong>Alingsas</strong>',
+		tooltip=tooltip).add_to(m),
+folium.Marker([57.708711, 11.974598], 
+		popup='<strong>Göteborg</strong>',
+		tooltip=tooltip,
+		icon=folium.Icon(color= 'green', icon='leaf')).add_to(m),
+		
+# Geojson overlay
+#folium.GeoJson(overlay, name='alingsas').add_to(m)
+folium.GeoJson(overlay, name='Alingsås').add_to(m)
+
+#Generate map - this will create an html-file to display file
+m.save('map.html')
+
+#open map.html in browser
